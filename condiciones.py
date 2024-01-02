7540f218d45967d77a6d4083a876e88c322a8752
Felicitas <felicitas.aguerralde@gmail.com>
2024-01-02 16:39:17 -0300

    agrego condicionales

---
+++ condiciones.py
@@ -0,0 +1,11 @@
+#Le agregue la funcion int para convertir el valor ingresado por el usuario a un número
+n = int(input("Number: "))
+
+
+#condicion + booleano + dos puntos + sangria con condicion
+if n>0:
+    print("El número ingresado es positivo")
+elif n<0:
+     print("El número ingresado es negativo")
+else:
+     print("El número ingresado es 0")
